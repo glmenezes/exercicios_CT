25138c885b7ef477ac72d834ace20a07519f7858
Thiago Feijó <tfeijo@gmail.com>
2021-06-22 21:26:11 -0300

    Exercicio 1

---
+++ main.py
@@ -0,0 +1,11 @@
+def exec01():
+  # Desenvolver um programa que pergunte ao usuário o seu nome 
+  # completo e seu sexo. Em seguida,
+  # o programa deve apresentar os dados anteriormente informados.
+  nome = input("Digite seu nome: ")
+  sexo = input("Digite seu sexo: ")
+
+  print(f"Seu nome é '{nome}' e seu sexo é '{sexo}'")
+
+
+exec01()
